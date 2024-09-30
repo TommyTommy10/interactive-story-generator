4ef04d2899b04012d5966ed1516ac33424044a49
TommyTommy10 <tommaso.grade@gmail.com>
2024-09-30 21:04:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import StoryContainer from './components/StoryContainer';
+import sampleStory from './stories/sampleStory.json';
 
+const App = () => {
+  return (
+    <div>
+      <h1>Interactive Story Generator</h1>
+      <StoryContainer story={sampleStory} />
+    </div>
+  );
+};
+
+export default App;
